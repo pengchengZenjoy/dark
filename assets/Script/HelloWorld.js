ee5840e2713ef13ac548d34db7b5949e5c4a9169
pengchengZenjoy <cheng.peng@zenjoy.net>
2018-08-17 15:25:29 +0800

    fisrt ci

---
+++ assets/Script/HelloWorld.js
@@ -0,0 +1,22 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        fire: cc.Sprite,
+        light1: cc.Sprite,
+        light2: cc.Sprite,
+        light3: cc.Sprite,
+        blackMask: cc.Sprite,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        var self = this;
+        //self.blackMask.set
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
